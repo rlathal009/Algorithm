a26a5a034919ab8d492c77e1c129779ee62d8b1e
Somi Kim <rlathal009@gmail.com>
2022-05-29 11:51:50 +0900

    BOJ 17298

---
+++ BAEKJOON/BOJ_17298.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_17298 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<Integer>();
+        int[] seq = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i< N; i++){
+            seq[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<N; i++){
+            while(!stack.isEmpty() && seq[stack.peek()] < seq[i]) {
+                seq[stack.pop()] = seq[i];
+            }
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()) {
+            seq[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<N; i++){
+            sb.append(seq[i]).append(' ');
+        }
+
+        System.out.println(sb);
+
+    }
+}
