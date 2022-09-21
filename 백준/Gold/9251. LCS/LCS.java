59917ad709a9b083741a49f14b9060f9de8552da
Somi Kim <rlathal009@gmail.com>
2022-09-21 15:27:18 +0900

    [Gold V] Title: LCS, Time: 152 ms, Memory: 18188 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -5,34 +5,28 @@ public class Main {
     static char[] str1;
     static char[] str2;
 
-    static Integer[][] dp;
-
+    static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         str1 = br.readLine().toCharArray();
         str2 = br.readLine().toCharArray();
 
-        dp = new Integer[str1.length][str2.length];
-        System.out.println(LCS(str1.length-1, str2.length-1));
-    }
+        int len1 = str1.length;
+        int len2 = str2.length;
 
-    private static int LCS(int x, int y) {
-        //index 범위 체크
-        if(x==-1 || y==-1) {
-            return 0;
-        }
+        dp = new int[len1+1][len2+1];
 
-        if(dp[x][y] == null){
-            dp[x][y] = 0;
-
-            if(str1[x] == str2[y]) {
-                dp[x][y] = LCS(x-1, y-1) + 1;
-            }
-            else{
-                dp[x][y] = Math.max(LCS(x-1, y), LCS(x, y-1));
+        for(int i=1; i<=len1; i++){
+            for(int j=1; j<=len2; j++){
+                if(str1[i-1] == str2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
+        System.out.println(dp[len1][len2]);
 
-        return dp[x][y];
     }
 }
