0f2d3be9f413f8d3cb86f99e4e72465a9ac7d54a
Somi Kim <rlathal009@gmail.com>
2022-10-04 22:41:11 +0900

    [level 3] Title: 야근 지수, Time: 115.48 ms, Memory: 68.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int work : works) {
+            pq.add(work);
+        }
+        
+        while(n>0) {
+            int work = pq.poll();
+            if(work == 0) break;
+            pq.add(--work);
+            n--;
+        }
+        
+        for(int num : pq) {
+            answer += Math.pow(num, 2);
+        }
+        
+        return answer;
+    }
+}
