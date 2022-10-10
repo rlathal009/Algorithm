836e73275a4adf229e54a9a1053d103036ae0758
Somi Kim <rlathal009@gmail.com>
2022-10-10 21:58:45 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 9.26 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        String temp = "";
+        
+        //k진법 변환
+        while(n!=0) {
+            temp = n%k + temp;
+            n /= k;
+        }
+        
+        String[] arr = temp.split("0");
+        
+        for(String data : arr) {
+            if(data.equals("")) continue;
+            long num = Long.parseLong(data);
+            if(isPrime(num)) answer++;
+        }
+        
+        return answer;
+    }
+    
+    public boolean isPrime(long n) {
+        if (n<2) return false;
+        for(int i=2; i<= Math.sqrt(n); i++) {
+            if(n%i == 0) return false;
+        }
+        
+        return true;
+    }
+}
