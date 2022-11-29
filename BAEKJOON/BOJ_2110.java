a38ea19a41edc2f519d4bce102268e0d17fe4cd2
Somi <rlathal009@gmail.com>
2022-11-29 23:53:51 +0900

    BOJ

---
+++ BAEKJOON/BOJ_2110.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2110 {
+    
+    public static int[] house;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        house = new int[N];
+
+        for(int i=0; i<N; i++) {
+            house[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(house);
+
+        int lo = 1;
+        int hi = house[N-1] - house[0] + 1;
+
+        while(lo<hi) {
+            int mid = (hi + lo) / 2;
+            if(canInstall(mid) < C) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        System.out.println(lo - 1);
+    }    
+
+    public static int canInstall(int distance) {
+        int count = 1;
+        int lastLocate = house[0];
+
+        for(int i=1; i<house.length; i++) {
+            int locate = house[i];
+
+            if(locate - lastLocate >= distance) {
+                count++;
+                lastLocate = locate;
+            }
+        }
+
+        return count;
+    }
+}
